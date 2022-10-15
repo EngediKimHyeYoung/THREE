c0b9b24135a052cfa0dce60f69442e07e3b1db18
KimHyeYoung <62800431+EngediKimHyeYoung@users.noreply.github.com>
2022-10-16 00:27:26 +0900

    10월 15일
    수정

--- dog/js/main.js
+++ dog/js/main.js
@@ -4,33 +4,31 @@ import * as THREE from 'three';
 // import {GLTFLoader} from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 // import * as THREE from '../node_modules/three/build/three.module.js';
 
-// console.log(GLTFLoader);
+// console.log(THREE);
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(30, 1);
-const renderer = new THREE.WebGL1Renderer({
-    canvas: document.querySelector("canvas"),
-    antialias: true
-});
-camera.position.set(0,0,5);
-renderer.outputEncoding = THREE.sRGBEncoding;
+const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer({canvas: document.querySelector("#box"), antialias: true, alpha: true});
+// console.log(renderer.domElement);
 scene.background = new THREE.Color("yellow");
-// renderer.render(scene, camera);
+camera.position.set(0, 0, 3.6); 
+renderer.setSize(700, 700);
+renderer.outputEncoding = THREE.sRGBEncoding;
+
 
 const loader = new GLTFLoader();
-
 loader.load("../shiba/scene.gltf", function(gltf){
-    // console.log(gltf);
-    scene.add(gltf.scene);
-    // renderer.render(scene, camera);
+    // console.log(gltf.scene);
+    scene.add(gltf.scene)
+
     function animate(){
         requestAnimationFrame(animate);
         gltf.scene.rotation.y += 0.01;
-        // gltf.scene.rotation.x += 0.01;
         renderer.render(scene, camera);
     }
     animate();
 });
+
 
 
 
